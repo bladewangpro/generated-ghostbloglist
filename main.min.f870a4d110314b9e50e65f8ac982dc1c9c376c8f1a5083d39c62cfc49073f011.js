9d1d790ccbf0a765ef062f4c3fe4491a27fae700
Tianyi's blog server <bladewangpro@outlook.com>
2021-06-07 09:50:20 +0000

    upload generated webpage

---
+++ main.min.f870a4d110314b9e50e65f8ac982dc1c9c376c8f1a5083d39c62cfc49073f011.js
@@ -0,0 +1,7 @@
+if(!!$.prototype.justifiedGallery){var options={rowHeight:140,margins:4,lastRow:"justify"};$(".article-gallery").justifiedGallery(options);}
+$(document).ready(function(){$("#header > #nav > ul > .icon").click(function(){$("#header > #nav > ul").toggleClass("responsive");});if($(".post").length){var menu=$("#menu");var nav=$("#menu > #nav");var menuIcon=$("#menu-icon, #menu-icon-tablet");if($(document).width()>=1440){menu.css("visibility","visible");menuIcon.addClass("active");}
+menuIcon.click(function(){if(menu.css("visibility")==="hidden"){menu.css("visibility","visible");menuIcon.addClass("active");}else{menu.css("visibility","hidden");menuIcon.removeClass("active");}
+return false;});if(menu.length){$(window).on("scroll",function(){var topDistance=menu.offset().top;if(!nav.is(":visible")&&topDistance<50){nav.show();}else if(nav.is(":visible")&&topDistance>100){nav.hide();}
+if(!$("#menu-icon").is(":visible")&&topDistance<50){$("#menu-icon-tablet").show();$("#top-icon-tablet").hide();}else if(!$("#menu-icon").is(":visible")&&topDistance>100){$("#menu-icon-tablet").hide();$("#top-icon-tablet").show();}});}
+if($("#footer-post").length){var lastScrollTop=0;$(window).on("scroll",function(){var topDistance=$(window).scrollTop();if(topDistance>lastScrollTop){$("#footer-post").hide();}else{$("#footer-post").show();}
+lastScrollTop=topDistance;$("#nav-footer").hide();$("#toc-footer").hide();$("#share-footer").hide();if(topDistance<50){$("#actions-footer > #top").hide();}else if(topDistance>100){$("#actions-footer > #top").show();}});}}});
